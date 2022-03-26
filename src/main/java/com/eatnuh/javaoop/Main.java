fb313dfc73db506a445f9103ad61f7ec2cc596a8
eatnuh <eatnuh@gamil.com>
2022-03-27 02:27:16 +0900

    Update #3 - Main
    변수 클래스를 인터페이스로 변경

--- src/main/java/com/eatnuh/javaoop/Main.java
+++ src/main/java/com/eatnuh/javaoop/Main.java
@@ -1,12 +1,13 @@
 package com.eatnuh.javaoop;
 
 import com.eatnuh.javaoop.logic.BubbleSort;
+import com.eatnuh.javaoop.logic.Sort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> bubbleSort = new BubbleSort<>();
+        Sort<String> bubbleSort = new BubbleSort<>();
 
         System.out.println("[Resulst] " + bubbleSort.sort(Arrays.asList(args)));
     }
