f6a246c122a8e4e7fd152e9ce048aa3683769c71
eatnuh <eatnuh@gamil.com>
2022-03-26 22:51:01 +0900

    Implement main entry point
    실행될 메인 로직 작성하고 정렬 알고리즘 사용

---
+++ src/test/java/com/eatnuh/javaoop/MainTest.java
@@ -0,0 +1,15 @@
+package com.eatnuh.javaoop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        //given
+        String[] args = {"3", "1", "2"};
+        Main.main(args);
+    }
+}
